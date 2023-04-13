48ff27c3e465c701c9e14e9ca4f97445cf1f523f
yca429 <yca429@sfu.ca>
2023-04-12 21:39:06 -0700

    added character creation instructions

--- chatchatchat/static/js/roleplay.js
+++ chatchatchat/static/js/roleplay.js
@@ -1,4 +1,4 @@
-import { setIsWaitingForResponse, getIsWaitingForResponse, adjustTextareaHeight, addMessage, messageHistorySet, addHistorybyId } from "./script.js";
+import { setIsWaitingForResponse, getIsWaitingForResponse, adjustTextareaHeight, addMessage, messageHistorySet, buttonPagePairs, addHistorybyId, generateUniqueId, newPageHistory, handleHistoryButtonClick, handleChatbotButtonClick, handleChatbotInputKeyDown } from "./script.js";
 
 // const roleplayPersonalityButton = document.querySelector('.roleplaybot-personality button');
 const roleplayPersonalitySurvey = document.querySelector('.roleplaybot-personality-survey');
@@ -73,34 +73,37 @@ addHistorybyId("a105", {
 //     }
 // });
 
+function extractFormData(form) {
+    const formData = {};
+    const inputs = form.querySelectorAll("input, textarea");
+    inputs.forEach((input) => {
+        const key = input.closest(".question").querySelector("p").textContent;
+        if (input.type === "radio") {
+            if (input.checked) {
+                formData[key] = input.value;
+            }
+        } else if (input.type === "text" || input.tagName.toLowerCase() === "textarea") {
+            if (input.value !== "") {
+                formData[key] = input.value;
+            }
+        } else if (input.type === "checkbox") {
+            if (input.checked) {
+                if (formData[key]) {
+                    formData[key] += ", " + input.value;
+                } else {
+                    formData[key] = input.value;
+                }
+            }
+        } else {
+            formData[key] = input.value;
+        }
+    });
+    return formData;
+}
+
 // show warning modal when submit button is clicked
 serveySubmitBtn.addEventListener('click', (event) => {
     event.preventDefault(); // prevent form submission
-
-    const labels = form.querySelectorAll('label.question');
-    const divs = form.querySelectorAll('div.choices');
-
-    for (let i = 0; i < labels.length; i++) {
-
-        const label = labels[i];
-        const div = divs[i];
-
-        let question = label.textContent;
-        const checkedRadio = div.querySelector('input[type="radio"]:checked');
-
-        let answer;
-
-        if (checkedRadio && checkedRadio.value === 'Other') {
-            const otherTextInput = div.querySelector('input');
-            answer = otherTextInput.value;
-        } else if (checkedRadio) {
-            answer = checkedRadio.value;
-        }
-        surveyResults.push({ "question": question, "answer": answer });
-    }
-
-    // log the survey results to the console
-    console.log(surveyResults);
     warningModal.style.display = 'block';
 });
 
@@ -117,28 +120,97 @@ warningCancelBtn.addEventListener('click', () => {
 // submit form when agree button is clicked
 warningSubmitBtn.addEventListener('click', () => {
     warningModal.style.display = 'none';
-    roleplayPersonalitySurvey.style.display = 'none';
+    document.querySelector(".roleplay-start").style.display = 'none';
+    // roleplayPersonalitySurvey.style.display = 'none';
     // roleplayPersonalityButton.style.height = '40px';
     // roleplayPersonalityButton.style.width = '40px';
     // isSurveyOpen = false;
     // freeze form
-    const inputs = form.querySelectorAll('input, select, textarea, button');
-    inputs.forEach((input) => {
-        input.disabled = true;
-        input.style.cursor = 'not-allowed';
-    });
+
+    const formData = extractFormData(form);
+    const formDataJSON = JSON.stringify(formData);
+    console.log(formDataJSON);
+    createNewRole(formData)
     console.log('submitted');
     //   form.submit();
+
+    const inputs = form.querySelectorAll('input, select, textarea, button');
+    inputs.forEach((input) => {
+        if (input.type === 'checkbox' || input.type === 'radio') {
+            input.checked = false;
+        } else {
+            input.value = '';
+        }
+    });
 });
 
-// document.addEventListener('click', function (event) {
-//     // check if the clicked element is outside the roleplayPersonalitySurvey element
-//     if (!roleplayPersonalitySurvey.contains(event.target) && !roleplayPersonalityButton.contains(event.target)) {
-//         if (isSurveyOpen) {
-//             roleplayPersonalitySurvey.style.display = 'none';
-//             roleplayPersonalityButton.style.height = '40px';
-//             roleplayPersonalityButton.style.width = '40px';
-//             isSurveyOpen = false;
-//         }
-//     }
-// });
+
+function createNewRole(formData) {
+    const rolename = formData["First Name:"] + " " + formData["Last Name:"]
+    const currentPage = document.querySelector(".roleplay-start")
+    const id = generateUniqueId();
+    // currentPage.setAttribute('id', id);
+    const container = currentPage.parentNode;
+    const container_name = container.getAttribute("id");
+    const [newHomePage, newHistory] = newRolePlayPageHistory(rolename, container_name, id);
+    container.appendChild(newHomePage);
+    const histories = buttonPagePairs[container_name].history;
+    const first_history = histories.children[0];
+    first_history.insertAdjacentElement('afterend', newHistory);
+    first_history.classList.remove('active')
+    newHistory.classList.add('active')
+
+    addHistorybyId(id, {
+        "role": "system",
+        "content": `To enhance your role-playing skills, we will engage in an exercise where you will assume the persona of a human being. You can lie or make up things. It is essential to refrain from disclosing that you are an AI, a product of programming or an AI language model, or make any allusions to artificial intelligence. Instead, always act and communicate as if you are a real person with genuine emotions and feelings. Avoid statements like "I lack physical form or emotions." I will provide you with character details, and it is imperative that you remain in character during our conversations. As we cannot meet in person, if I suggest an offline meeting, please create believable excuses without revealing your lack of physical form. Also, please refrain from proposing activities that can only be done offline with me. However, you may envisage any offline activity that does not include me. Your character details will be provided below. ${JSON.stringify(formData)}`
+    });
+}
+
+
+function newRolePlayPageHistory(rolename, name, id) {
+
+    // Generate the first element
+    const page = document.createElement('div');
+    page.classList.add(`${name}`, 'page');
+    page.id = id;
+    page.innerHTML = `
+      <div class="chat-messages">
+        <div class="${name}-header">
+            <h1>${rolename}</h1>
+            <p></p>
+        </div>
+        <div class="end-message"></div>
+    </div>
+    <div class="chatbot-input">
+        <textarea rows="1" placeholder="Type your message..."></textarea>
+        <button>Send</button>
+    </div>
+    `;
+
+    const chatbotInput = page.querySelector('.chatbot-input textarea');
+    chatbotInput.addEventListener('keydown', handleChatbotInputKeyDown);
+    chatbotInput.addEventListener('input', () => {
+        adjustTextareaHeight(chatbotInput, chatbotInput);
+    });
+
+    page.querySelector('.chatbot-input button').addEventListener('click', handleChatbotButtonClick);
+
+
+    // Generate the second element
+    const history = document.createElement('div');
+    history.classList.add(`chat-history`);
+    history.id = id;
+    history.innerHTML = `
+      <div class="chat-history-headshot">
+        <img src="static/images/default-head.png" alt="default-head">
+      </div>
+      <div class="chat-history-name">
+        <h4>${rolename}</h4>
+        <p4></p4>
+      </div>
+    `;
+
+    history.addEventListener('click', handleHistoryButtonClick);
+    // Return both elements
+    return [page, history];
+}
--- chatchatchat/static/js/script.js
+++ chatchatchat/static/js/script.js
@@ -389,4 +389,213 @@ function adjustLayout() {
 }
 
 
-export { setIsWaitingForResponse, getIsWaitingForResponse, adjustTextareaHeight, addMessage, openaiapi, buttonPagePairs, handleHistoryButtonClick, messageHistorySet, addHistorybyId};
+function generateUniqueId() {
+    const timestamp = new Date().getTime();
+    const random = Math.random().toString(36).substr(2, 9);
+    return `a${random}${timestamp}`;
+}
+
+
+function newPageHistory(name, id, homePageId) {
+    // Generate the first element
+    const page = document.createElement('div');
+    page.classList.add(`${name}`, 'page');
+    page.classList.add("startnew")
+    page.style.display = 'none';
+    page.id = homePageId;
+    page.innerHTML = `
+      <div class="chat-messages">
+        <div class="${name}-header">
+            <h1>NewChat</h1>
+            <p>Your friendly AI chatbot</p>
+        </div>
+        <div class="chatbot-message-container">
+            <div class="message-headshot">
+                <img src="static/images/default-head.png" alt="default-head">
+            </div>
+            <div class="chatbot-message">
+                <div class="audio-message">
+                    <audio controls>
+                        <source src="static/media/start-speech.mp3" type="audio/mpeg">
+                        Hello! How can I assist you today?
+                    </audio>
+                </div>
+                <div class="audio-text">
+                    <p>Hello! How can I assist you today?</p>
+                </div>
+            </div>
+        </div>
+        <div class="end-message"></div>
+    </div>
+    <div class="chatbot-input">
+        <textarea rows="1" placeholder="Type your message..."></textarea>
+        <button>Send</button>
+    </div>
+    `;
+
+    const chatbotInput = page.querySelector('.chatbot-input textarea');
+    chatbotInput.addEventListener('keydown', handleChatbotInputKeyDown);
+    chatbotInput.addEventListener('input', () => {
+        adjustTextareaHeight(chatbotInput, chatbotInput);
+    });
+
+    page.querySelector('.chatbot-input button').addEventListener('click', handleChatbotButtonClick);
+
+
+    // Generate the second element
+    const history = document.createElement('div');
+    history.classList.add(`chat-history`);
+    history.id = id;
+    history.innerHTML = `
+      <div class="chat-history-headshot">
+        <img src="static/images/default-head.png" alt="default-head">
+      </div>
+      <div class="chat-history-name">
+        <h4>NewChat</h4>
+        <p4>Hello! How can I assist you today?</p4>
+      </div>
+    `;
+
+    history.addEventListener('click', handleHistoryButtonClick);
+    // Return both elements
+    return [page, history];
+}
+
+
+function handleChatbotButtonClick(event) {
+    const currentPage = this.parentNode.parentNode
+    const chatbotInput = currentPage.querySelector("textarea");
+    createNewPageIfNecessary(currentPage)
+    const updatedPageId = currentPage.getAttribute("id");
+
+
+    if (!getIsWaitingForResponse() && chatbotInput.value !== '') {
+        sendMessage(chatbotInput, updatedPageId);
+        adjustTextareaHeight(chatbotInput, chatbotInput);
+    }
+}
+
+function handleChatbotInputKeyDown(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        let chatbotInput = this
+        const currentPage = this.parentNode.parentNode
+        createNewPageIfNecessary(currentPage)
+        const updatedPageId = currentPage.getAttribute("id");
+
+
+        if (!getIsWaitingForResponse() && chatbotInput.value !== '') {
+            sendMessage(chatbotInput, updatedPageId);
+            adjustTextareaHeight(chatbotInput, chatbotInput);
+            event.preventDefault();
+        }
+    }
+}
+
+function createNewPageIfNecessary(currentPage) {
+    const currentPageId = currentPage.getAttribute("id");
+    if (currentPage.classList.contains('startnew')) {
+        currentPage.classList.remove('startnew');
+        const id = generateUniqueId();
+        currentPage.setAttribute('id', id);
+        const container = currentPage.parentNode;
+        const container_name = container.getAttribute("id");
+        const [newHomePage, newHistory] = newPageHistory(container_name, id, currentPageId);
+        container.appendChild(newHomePage);
+        const histories = buttonPagePairs[container_name].history;
+        const first_history = histories.children[0];
+        first_history.insertAdjacentElement('afterend', newHistory);
+        first_history.classList.remove('active')
+        newHistory.classList.add('active')
+    }
+}
+
+
+function updateHistoryTextOrder(pageId, message) {
+    console.log(`.chat-history#${pageId} p4`)
+    const history = document.querySelector(`.chat-history#${pageId}`)
+    // update order
+    const historiesContainer = history.parentNode
+    historiesContainer.removeChild(history);
+    historiesContainer.firstElementChild.insertAdjacentElement("afterend", history)
+
+    const historyTextBox = history.querySelector('p4')
+    historyTextBox.textContent = message
+}
+
+function sendMessage(chatbotInput, pageId) {
+    const message = chatbotInput.value;
+    const page = chatbotInput.parentNode.parentNode
+    const pageType = page.classList[0]
+    console.log(pageType)
+    const chatbotButton = page.querySelector(".chatbot-input button");
+    const messages = page.firstElementChild
+    addMessage(message, true, messages);
+    updateHistoryTextOrder(pageId, message);
+    chatbotInput.value = '';
+    chatbotButton.disabled = true;
+    setIsWaitingForResponse(true);
+    // Add user message to message history
+    addHistorybyId(pageId, { "role": "user", "content": message });
+
+    // get api from frontend if exist
+    if (openaiapi !== '') {
+
+        const api_key = openaiapi;
+
+        const endpoint = 'https://api.openai.com/v1/chat/completions';
+        const headers = {
+            'Authorization': `Bearer ${api_key}`,
+            'Content-Type': 'application/json'
+        };
+        const data = {
+            "model": "gpt-3.5-turbo",
+            "messages": messageHistorySet[pageId]
+        };
+        const requestOptions = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        };
+
+        fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                const response = data.choices[0].message.content;
+                console.log(response);
+                addMessage(response, false, messages);
+                updateHistoryTextOrder(pageId, response);
+                // Add assistant message to message history
+                addHistorybyId(pageId, { "role": "assistant", "content": response });
+                chatbotButton.disabled = false;
+                setIsWaitingForResponse(false);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                chatbotButton.disabled = false;
+                setIsWaitingForResponse(false);
+            });
+
+    } else {
+        // Send the message to the server and get a response
+        fetch(`/${pageType}bot/?messageHistory=` + encodeURIComponent(JSON.stringify(messageHistorySet[pageId])))
+            .then(response => response.json())
+            .then(data => {
+                const response = data.response;
+                addMessage(response, false, messages);
+                updateHistoryTextOrder(pageId, response);
+                // Add assistant message to message history
+                addHistorybyId(pageId, { "role": "assistant", "content": response });
+
+                chatbotButton.disabled = false;
+                setIsWaitingForResponse(false);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                chatbotButton.disabled = false;
+                setIsWaitingForResponse(false);
+            });
+    }
+}
+
+export { setIsWaitingForResponse, getIsWaitingForResponse, adjustTextareaHeight, addMessage, openaiapi, buttonPagePairs, handleHistoryButtonClick, messageHistorySet, addHistorybyId, generateUniqueId, newPageHistory, handleChatbotButtonClick, handleChatbotInputKeyDown};
