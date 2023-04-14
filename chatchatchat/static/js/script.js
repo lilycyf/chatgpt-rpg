9690b472b7f39f0908986c38eff02898cb6cedc7
yca429 <yca429@sfu.ca>
2023-04-14 00:46:19 -0700

    added warning message at top when error accrue

--- chatchatchat/static/js/script.js
+++ chatchatchat/static/js/script.js
@@ -576,7 +576,8 @@ function sendMessage(chatbotInput, pageId) {
                 setIsWaitingForResponse(false);
             })
             .catch(error => {
-                console.error('Error:', error);
+                // console.error('Error:', error);
+                showTopError(error.message);
                 chatbotButton.disabled = false;
                 setIsWaitingForResponse(false);
             });
@@ -596,11 +597,35 @@ function sendMessage(chatbotInput, pageId) {
                 setIsWaitingForResponse(false);
             })
             .catch(error => {
-                console.error('Error:', error);
+                // console.error('Error:', error);
+                showTopError(error.message);
                 chatbotButton.disabled = false;
                 setIsWaitingForResponse(false);
             });
     }
+}
+
+function showTopError(text){
+    const errorMessage = document.createElement('p');
+                errorMessage.textContent = text;
+                errorMessage.style.backgroundColor = '#e9e9e9';
+                errorMessage.style.width = "60%"
+                errorMessage.style.padding = '10px';
+                errorMessage.style.borderRadius = "5px";
+                errorMessage.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.3)";
+                errorMessage.style.position = 'fixed';
+                errorMessage.style.top = '20px';
+                errorMessage.style.left = '50%';
+                errorMessage.style.transform = 'translateX(-50%)';
+                errorMessage.style.zIndex = '9999';
+                document.body.appendChild(errorMessage);
+                setTimeout(() => {
+                    errorMessage.style.transition = 'opacity 1s ease-in-out';
+                    errorMessage.style.opacity = '0';
+                    setTimeout(() => {
+                        errorMessage.parentNode.removeChild(errorMessage);
+                    }, 1000);
+                }, 2000);
 }
 
 export { setIsWaitingForResponse, getIsWaitingForResponse, adjustTextareaHeight, addMessage, openaiapi, buttonPagePairs, handleHistoryButtonClick, messageHistorySet, addHistorybyId, generateUniqueId, newPageHistory, handleChatbotButtonClick, handleChatbotInputKeyDown };
